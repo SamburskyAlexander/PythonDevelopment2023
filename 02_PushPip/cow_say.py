f45f1020df30addbad2aa374694ec7e024a4fcc5
SamburskyAlexander <sashasamb@yandex.ru>
2023-02-19 01:19:44 +0300

    Added call of cowsay function

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,15 +1,37 @@
+import sys
 import argparse
+from cowsay import cowsay
 
+
+# TODO: add -l option
 
 def main(args):
-    # TODO: add call of cowsay function
-    pass
+    line_list = []
+    for input_line in sys.stdin:
+        line_list.append(input_line.strip())
+    message = "\n".join(line_list)
+
+    # TODO: add set value of preset
+
+    print(cowsay(
+        message=message,
+        cow=args.f,
+        preset=None,  # use default value for now
+        eyes=args.e,
+        tongue=args.T,
+        width=args.W,
+        wrap_text=not args.n)
+    )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='cowsay implementation')
 
-    # TODO: add arguments for parser
+    parser.add_argument("-e", type=str, default="oo", help="eyes")
+    parser.add_argument("-f", type=str, default="default", help="cow file")
+    parser.add_argument("-n", action="store_true", help="arbitrary whitespaces")
+    parser.add_argument("-T", type=str, default='  ', help="tongue")
+    parser.add_argument("-W", type=int, default=40, help="message width")
 
     args = parser.parse_args()
 
