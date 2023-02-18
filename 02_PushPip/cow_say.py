0477efd724fbd454d7b4a63b7edcc2dc1e2f530f
SamburskyAlexander <sashasamb@yandex.ru>
2023-02-19 01:41:43 +0300

    Update params help & little changes

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -27,19 +27,20 @@ def main(args):
             eyes=args.e,
             tongue=args.T,
             width=args.W,
-            wrap_text=not args.n)
+            wrap_text=not args.n),
+            file=sys.stdout
         )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='cowsay implementation')
 
-    parser.add_argument("-e", type=str, default="oo", help="eyes")
-    parser.add_argument("-f", type=str, default="default", help="cow file")
+    parser.add_argument("-e", type=str, default='oo', help="string for eyes")
+    parser.add_argument("-f", type=str, default='default', help="cow picture file")
     parser.add_argument("-l", action="store_true", help='show list of cows')
-    parser.add_argument("-n", action="store_true", help="arbitrary whitespaces")
-    parser.add_argument("-T", type=str, default='  ', help="tongue")
-    parser.add_argument("-W", type=int, default=40, help="message width")
+    parser.add_argument("-n", action="store_true", help='message with arbitrary whitespaces')
+    parser.add_argument("-T", type=str, default='  ', help='string for a tongue')
+    parser.add_argument("-W", type=int, default=40, help='width of message picture')
 
     for option in OPTIONALS:
         parser.add_argument(f"-{option}", action="store_true")
