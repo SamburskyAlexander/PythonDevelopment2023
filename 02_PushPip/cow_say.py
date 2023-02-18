2c1bbef7a492c5faee2da4219c5b9edb04c465d5
SamburskyAlexander <sashasamb@yandex.ru>
2023-02-19 01:03:32 +0300

    Add blank cowsay file

---
+++ 02_PushPip/cow_say.py
@@ -0,0 +1,16 @@
+import argparse
+
+
+def main(args):
+    # TODO: add call of cowsay function
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='cowsay implementation')
+
+    # TODO: add arguments for parser
+
+    args = parser.parse_args()
+
+    main(args)
