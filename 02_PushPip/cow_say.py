f32557f92dad15ae16cdb0375d10a4ad0e3138a7
SamburskyAlexander <sashasamb@yandex.ru>
2023-02-19 01:36:39 +0300

    Add -l parameter launch

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,33 +1,34 @@
 import sys
 import argparse
-from cowsay import cowsay
-
-# TODO: add -l option
+from cowsay import cowsay, list_cows
 
 OPTIONALS = set("bdgpstwy")
 
-
 def main(args):
-    line_list = []
-    for input_line in sys.stdin:
-        line_list.append(input_line.strip())
-    message = "\n".join(line_list)
+    if args.l:
+        print(list_cows())
 
-    preset = None
-    for option, value in args._get_kwargs():
-        if option in OPTIONALS and value:
-            preset = option
-            break
+    else:
+        line_list = []
+        for input_line in sys.stdin:
+            line_list.append(input_line.strip())
+        message = "\n".join(line_list)
 
-    print(cowsay(
-        message=message,
-        cow=args.f,
-        preset=preset,
-        eyes=args.e,
-        tongue=args.T,
-        width=args.W,
-        wrap_text=not args.n)
-    )
+        preset = None
+        for option, value in args._get_kwargs():
+            if option in OPTIONALS and value:
+                preset = option
+                break
+
+        print(cowsay(
+            message=message,
+            cow=args.f,
+            preset=preset,
+            eyes=args.e,
+            tongue=args.T,
+            width=args.W,
+            wrap_text=not args.n)
+        )
 
 
 if __name__ == '__main__':
@@ -35,6 +36,7 @@ if __name__ == '__main__':
 
     parser.add_argument("-e", type=str, default="oo", help="eyes")
     parser.add_argument("-f", type=str, default="default", help="cow file")
+    parser.add_argument("-l", action="store_true", help='show list of cows')
     parser.add_argument("-n", action="store_true", help="arbitrary whitespaces")
     parser.add_argument("-T", type=str, default='  ', help="tongue")
     parser.add_argument("-W", type=int, default=40, help="message width")
